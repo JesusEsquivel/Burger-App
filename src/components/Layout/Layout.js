c1438c551aa7579a7800ab2f8b7f910e7244fc5a
Jesus Esquivel <jesqivela@gmail.com>
2018-03-18 20:17:04 -0400

    Add files via upload

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state= {
+        showSideDrawer: true
+    }
+
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.SideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
